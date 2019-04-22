01ffa2d9dbcb213f6c9180a757ad6ee471bd889d
Christian Steimel <me@C14L.com>
2019-04-22 14:51:46 +0200

    Initial redis access

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+extern crate redis;
 #[macro_use]
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 
+use redis::Commands;
 use rocket::http::Method;
 use rocket::{get, routes};
 use rocket_contrib::json::{Json, JsonValue};
@@ -27,12 +29,29 @@ struct SrState {
 }
 
 #[get("/pics.json", format = "json", rank = 1)]
-fn pics_get() -> JsonValue {
+fn pics_get() -> Result<JsonValue, Error> {
+    let client = redis::Client::open("redis://127.0.0.1:6379/").unwrap();
+    let con = client.get_connection().unwrap();
+    let result: Vec<String> = con.lrange("p1", 0, -1).unwrap();
+    let pics: Vec<UserPic> = result.iter().map(|x| UserPic { url: x.clone() }).collect();
+    Ok(json!(pics))
+}
+
+#[post("/pics.json", format = "json", rank = 3)]
+fn pics_post() -> JsonValue {
     let pics = vec![
-        UserPic { url: String::from("https://i.imgur.com/jbaAGJHm.jpg") },
-        UserPic { url: String::from("https://i.imgur.com/vPrly7Dm.jpg") },
-        UserPic { url: String::from("https://i.imgur.com/HdIW12nm.jpg") },
-        UserPic { url: String::from("https://i.imgur.com/GewGxsom.jpg") },
+        UserPic {
+            url: String::from("https://i.imgur.com/jbaAGJHm.jpg"),
+        },
+        UserPic {
+            url: String::from("https://i.imgur.com/vPrly7Dm.jpg"),
+        },
+        UserPic {
+            url: String::from("https://i.imgur.com/HdIW12nm.jpg"),
+        },
+        UserPic {
+            url: String::from("https://i.imgur.com/GewGxsom.jpg"),
+        },
     ];
     json!(pics)
 }
@@ -63,7 +82,8 @@ fn srlist_post(msg: Json<SrState>) -> JsonValue {
 }
 
 fn main() -> Result<(), Error> {
-    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:8001"]);
+    let allowed_origins =
+        AllowedOrigins::some_exact(&["http://localhost:8000", "http://localhost:8001"]);
     let mut html_pages: HashMap<OsString, String> = HashMap::new();
     pages::preload_static_pages(&mut html_pages);
 
@@ -80,7 +100,7 @@ fn main() -> Result<(), Error> {
     .to_cors()?;
 
     rocket::ignite()
-        .mount("/api/v2", routes![srlist_get, srlist_post, pics_get])
+        .mount("/api/v2", routes![srlist_get, srlist_post, pics_get, pics_post])
         .mount("/", routes![pages::home, pages::settings])
         .attach(cors)
         .manage(html_pages)
--- src/pages.rs
+++ src/pages.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 use std::env;
 use std::ffi::{OsStr, OsString};
 use std::fs;
-use std::path::{PathBuf};
+use std::path::PathBuf;
 
 pub fn fetch_pages_filenames() -> Vec<fs::DirEntry> {
     // only get .html files
@@ -18,19 +18,17 @@ pub fn fetch_pages_filenames() -> Vec<fs::DirEntry> {
         Err(_) => PathBuf::from("/tmp"),
     };
     // get only files with the correct extension
-    pagesdir.read_dir().expect("Could not read pages dir.")
+    pagesdir
+        .read_dir()
+        .expect("Could not read pages dir.")
         .filter_map(|file| file.ok())
-        .filter(|file| {
-            match file.file_type() {
-                Ok(filetype) => filetype.is_file(),
-                Err(_) => false,
-            }
+        .filter(|file| match file.file_type() {
+            Ok(filetype) => filetype.is_file(),
+            Err(_) => false,
         })
-        .filter(|file| {
-            match file.path().extension() {
-                Some(ext) => html_ext == ext,
-                _ => false,
-            }
+        .filter(|file| match file.path().extension() {
+            Some(ext) => html_ext == ext,
+            _ => false,
         })
         .collect()
 }
