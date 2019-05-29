0043d320568c7764c4b4431a26ca19b6f01e9328
Christian Steimel <me@C14L.com>
2019-05-29 13:38:26 +0200

    Add AuthSession struct

--- src/redditauth.rs
+++ src/redditauth.rs
@@ -3,6 +3,8 @@
 
 extern crate reqwest;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use reqwest::Url;
 use rocket::http::{Cookie, Cookies};
 use rocket::request::Form;
@@ -31,14 +33,10 @@ pub struct RedditAccessToken {
     expires_in: usize,
     refresh_token: String,
     scope: String,
+    update_time: Option<u64>,
+    expire_time: Option<u64>,
+    create_time: Option<u64>,
 }
-// {
-//     "access_token": "46597193-8-RsLVzf98n3OJFNZEXXXfFJddU",
-//     "token_type": "bearer",
-//     "expires_in": 3600,
-//     "refresh_token": "46597193-pu-dfgzJByDgp_qCvXXX6hAlTtE",
-//     "scope": "identity"
-// }
 
 impl RedditAccessToken {
 
@@ -55,6 +53,13 @@ impl RedditAccessToken {
             .form(&body)
             .send()?
             .json()
+            .and_then(|mut x: RedditAccessToken| {
+                let t = SystemTime::now().duration_since(UNIX_EPOCH).expect("No time?").as_secs();
+                x.create_time = Some(t.clone());
+                x.update_time = Some(t.clone());
+                x.expire_time = Some(t.clone() + x.expires_in.clone() as u64);
+                Ok(x)
+            })
     }
 
     pub fn refresh(self) -> Result<RedditAccessToken, reqwest::Error> {
@@ -68,14 +73,57 @@ impl RedditAccessToken {
             .form(&body)
             .send()?
             .json()
+            .and_then(|mut x: RedditAccessToken| {
+                let t = SystemTime::now().duration_since(UNIX_EPOCH).expect("No time?").as_secs();
+                x.update_time = Some(t.clone());
+                x.expire_time = Some(t.clone() + x.expires_in.clone() as u64);
+                Ok(x)
+            })
     }
 }
 
+#[derive(Debug, Deserialize)]
+pub struct AuthSession {
+    sess_id: String,
+    token: RedditAccessToken,
+}
+
+impl AuthSession {
+    pub fn new(sess_id: &str, token: RedditAccessToken) -> AuthSession {
+        AuthSession {
+            sess_id: sess_id.into(),
+            token: token,
+        }
+    }
+}
+
+
 // Simply redirects the client to Reddit's oAuth page.
 #[get("/redditauth.html")]
-pub fn oauth_call_get() -> Result<Redirect, NotFound<String>> {
+pub fn oauth_call_get(mut cookies: Cookies) -> Result<Redirect, NotFound<String>> {
     // TODO: Generate a `state` uuid and remember it to verify in oauth_callback.
     // TODO: Load `client_id` and `redirect_uri` from some conf file.
+
+    let _sess_cookie = match cookies.get_private("rmrs_sessid") {
+        Some(cookie) => {
+            // There is a session cookie, check if it has a valid token in Redis.
+
+            // If it does, try to refresh it.
+
+            // If it refreshes, auth was sucessful --> redir to app.
+
+            // If any of that fails, reuse cookie and we need to go through oauth flow.
+            cookie
+        },
+        None => {
+            // No sess cookie found, create one, then go through oauth flow
+            let random_uuid = "a8yr7ersdfsd4iuyhli54-=09u8hfi3;fdk-0";
+            let cookie = Cookie::new("rmrs_sessid", random_uuid);
+            cookies.add_private(cookie.clone());
+            cookie
+        }
+    };
+
     Ok(Redirect::to(format!(
         "https://ssl.reddit.com/api/v1/authorize?\
          duration=permanent&response_type=code&scope=identity&\
@@ -91,36 +139,35 @@ pub fn oauth_callback_get(
 ) -> Result<Html<String>, NotFound<String>> {
     println!("THIS IS: oauth_callback_get()");
 
-    let sess_cookie = match cookies.get_private("rmrs_sessid") {
-        Some(c) => c,
-        None => {
-            // TODO: Generate a cookie uuid here.
-            let c = Cookie::new("rmrs_sessid", "a8yr7ersdfsd4iuyhli54-=09u8hfi3;fdk-0");
-            cookies.add_private(c.clone());
-            c
-        }
-    };
+    let sess_cookie = cookies.get_private("rmrs_sessid").unwrap_or_else(|| {
+        // This is weird, because a session should have been created before
+        // before the oauth redirect. But for now, just go with it.
+        let random_uuid = "a8yr7ersdfsd4iuyhli54-=09u8hfi3;fdk-0";
+        let cookie = Cookie::new("rmrs_sessid", random_uuid);
+        cookies.add_private(cookie.clone());
+        cookie
+    });
     println!("COOKIE: {:?}", &sess_cookie);
 
-    let token: Result<RedditAccessToken, reqwest::Error> = match &params.code {
-        Some(code) => RedditAccessToken::new(code.to_string()),
-        None => return Err(NotFound("Invalid code.".to_string())),
-    };
-    println!("TOKEN RECEIVED: {:?}", &token);
+    let token: Option<RedditAccessToken> = params.code.clone().and_then(
+        |c| RedditAccessToken::new(c).ok());
+    let sess: Option<AuthSession> = token.and_then(
+        |t| Some(AuthSession::new(sess_cookie.value(), t)));
+    println!("SESS: {:?}", &sess);
 
-    match token {
-        Ok(t) => {
+    match sess {
+        Some(s) => {
             Ok(Html(format!(
-                "cookie: '{}' and state: '{}' -- access_token: '{}', token_type: '{}', expires_in: '{}', scope: '{}', refresh_token: '{}'.",
-                sess_cookie,
+                "session_id: '{}' and state: '{}' -- access_token: '{}', token_type: '{}', expires_in: '{}', scope: '{}', refresh_token: '{}'.",
+                sess_cookie.value(),
                 params.state.clone().unwrap_or_default(),
-                t.access_token,
-                t.token_type,
-                t.expires_in,
-                t.scope,
-                t.refresh_token,
+                s.token.access_token,
+                s.token.token_type,
+                s.token.expires_in,
+                s.token.scope,
+                s.token.refresh_token,
             )))
         },
-        Err(_) => Err(NotFound("Invalid token.".to_string()))
+        None => Err(NotFound("Invalid token.".to_string()))
     }
 }
--- src/session.rs
+++
@@ -1,5 +0,0 @@
-// /// Set and get values from a user's session store.
-
-// use rocket::http::{Cookie, Cookies};
-
-// pub fn
